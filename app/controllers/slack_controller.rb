1bd34bdf11c6c3e87f49a9ba85754df3bebfec9b
Chisato Matoba <chisato.matoba@luxiar.com>
2024-05-14 23:18:59 +0900

    スレッド取得仮

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,9 +1,24 @@
 class SlackController < ApplicationController
-  def fetch_thread
-    client = Slack::Web::Client.new
-    @messages = client.conversations_replies(
-      channel: params[:channel_id],
-      ts: params[:thread_ts]
-    ).messages
+  def index
+  end
+
+  def fetch_thread_from_url
+    url = params[:thread_url]
+    matches = url.match(/archives\/(?<channel_id>C\w+)\/p(?<timestamp>\d+)/)
+    puts "matches: #{matches[:channel_id]}, #{matches[:timestamp]}"
+    if matches
+      formatted_timestamp = matches[:timestamp].insert(10, '.')
+      puts "matches: #{matches[:channel_id]}, #{formatted_timestamp}"
+      client = Slack::Web::Client.new
+      @messages = client.conversations_replies(
+        token: ENV['SLACK_SCOPE_TOKEN'],
+        channel: matches[:channel_id],
+        ts: formatted_timestamp
+      ).messages
+      render :index
+    else
+      flash[:alert] = "Invalid URL format."
+      redirect_to slack_index_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
 
-  get '/fetch_thread', to: 'slack#fetch_thread'
-
+  get '/slack/index', to: 'slack#index'
+  post 'slack/fetch_thread_from_url', to: 'slack#fetch_thread_from_url'
 end

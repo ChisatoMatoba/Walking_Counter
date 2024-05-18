1c5581cccb8e1e53c852d7aaa9afac7397f96678
Chisato Matoba <chisato.matoba@luxiar.com>
2024-05-18 23:22:20 +0900

    名前修正

--- app/models/slack_thread.rb
+++ app/models/slack_thread.rb
@@ -6,15 +6,23 @@ class SlackThread < ApplicationRecord
       unless self.slack_posts.exists?(post_id: message.ts)
         user_info = get_user_info(message.user)
         image_urls = message.files&.map { |file| file.url_private_download } if message.files&.any?
-
         slack_post_params = {
           post_id: message.ts,
-          author_name: user_info.name,
+          author_name: user_info.user.profile.display_name,
           content: message.text,
           posted_at: Time.at(message.ts.to_f),
           image_urls: image_urls
         }
         self.slack_posts.create(slack_post_params)
+      end
+    end
+  end
+
+  def save_images
+    self.slack_posts.each do |post|
+      post.image_urls&.each do |image_url|
+        image = Image.new
+        image.download_and_store_image(post, image_url)
       end
     end
   end
@@ -36,14 +44,5 @@ class SlackThread < ApplicationRecord
       token: ENV['SLACK_SCOPE_TOKEN'],
       user: user_id
     )
-  end
-
-  def save_images
-    self.slack_posts.each do |post|
-      post.image_urls&.each do |image_url|
-        image = Image.new
-        image.download_and_store_image(post, image_url)
-      end
-    end
   end
 end
